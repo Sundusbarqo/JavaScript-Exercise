7ef18acf9324d3d47c45add378939e5a95cda99d
SUNDUS <calamiinwali@gmail.com>
2024-08-28 21:39:16 +0300

    Add files via upload

---
+++ Day1/DataType.js
@@ -0,0 +1,78 @@
+// Different dataType
+
+// question 5
+
+let number = 42;
+console.log(number)
+
+let String = "Sundus";
+console.log(String)
+
+let boolean = true; 
+console.log(boolean)
+
+let age = 20;
+console.log(age)
+
+let array = [1, 2, 3]
+console.log(array)
+
+ // question 6
+
+// without assigning values
+let x;
+console.log(x)
+
+let y;
+console.log(y)
+
+let z;
+console.log(z)
+
+let r;
+console.log(r)
+
+ // question 7
+
+// with assigning values
+let sum = 2
+console.log(sum);
+
+let a = 4 / 3
+console.log(a)
+
+let q = 4 + 4
+console.log(q)
+
+let t = 33 * 33
+console.log(t)
+
+ // question 8
+
+//variable store multiple line
+let firstName = "Sundus"
+let  lastName = "Ahmed"
+let  foodFavor = "pasta"
+let country  = "Somali"
+
+console.log(firstName)
+console.log(lastName)
+console.log(foodFavor)
+console.log(country)
+
+// question 9
+
+//variable store single line
+let b = 33;  t = 32; j = 40;
+console.log(b, t, j)
+
+
+
+
+// quetion 10
+
+//assigning values age
+let myAge = 50
+let yourAge = 44
+
+console.log(myAge, yourAge)
---
+++ Day1/Variable.js
@@ -0,0 +1,26 @@
+// 1 = Comment can make code readable
+
+//  2 = Welcome to 30Days of JavaSript
+
+// 3 = Comment can make code readable easy to reuse and informative
+ 
+//question 4
+
+//string
+let name = "Sundus"
+console.log(name)
+
+//boolean
+let isStudent =  true
+console.log(isStudent)
+
+let isTeacher = false
+console.log(isTeacher)
+
+//undifined
+let x;
+console.log(x)
+
+// null
+let y = null
+console.log(y)
